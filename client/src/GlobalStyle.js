05a91fd08223b7435f1f17761c93149c21e094ce
Matt-1985 <blatsche@web.de>
2020-11-26 16:27:02 +0100

    💄 Add styled components and globalStyle.js #4

---
+++ client/src/GlobalStyle.js
@@ -0,0 +1,52 @@
+import { createGlobalStyle } from "styled-components";
+import AtamiRegular from "./stories/assets/fonts/AtamiRegular.otf";
+import AtamiBold from "../src/stories/assets/fonts/AtamiBold.otf";
+
+const GlobalStyle = createGlobalStyle`
+ *, *::after, *::before {
+   box-sizing:border-box;
+ }
+
+
+ @font-face {
+  font-family: "AtamiRegular";
+  font-style: normal;
+  font-weight: 400;
+  src: url(${AtamiRegular});
+ }
+
+  @font-face {
+  font-family: "AtamiBold";
+  font-style: bold;
+  font-weight: 600;
+  src: url(${AtamiBold})
+}
+ 
+ :root {
+   --primary-color: #fbfbfb;
+   --guidance-color: #0a335c;
+   --decision-primary-color: #ff4477;
+   --decision-1-color: #ffd17e;
+   --decision-filter-color: #20f6ce;
+   --input-placeholder-color: #607A94;
+}
+
+body{
+margin: 0;
+font-size: 18;
+font-family: "AtamiRegular";
+-webkit-font-smoothing: antialiased;
+-moz-osx-font-smoothing: grayscale;
+color: var(--guidance-color);
+
+}
+
+h1 {
+font-family: "AtamiBold";
+font-size: 23;
+
+}
+ 
+`;
+
+export default GlobalStyle;
