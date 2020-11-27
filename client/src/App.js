76dcf161f5da78163ef8e2378ecb6b8dd13c63ae
Matt-1985 <blatsche@web.de>
2020-11-27 14:27:47 +0100

    🎨  Build SplashPage and adjust storybook settings

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,12 @@
 import React from "react";
 import GlobalStyle from "./GlobalStyle";
-import logo from "./stories/assets/logo.svg";
+import SplashPage from "./pages/SplashPage";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
-      <img src={logo} className="app_logo" alt="choicely" />
-      <a href="/storybook">To Storybook</a>
+      <SplashPage />
     </div>
   );
 }
