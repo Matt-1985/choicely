e896ea3d870e5cc86849159621678dce3bf3e93e
Matt-1985 <blatsche@web.de>
2020-11-30 17:18:31 +0100

    💄 Add First Action Button & styling

---
+++ client/src/components/FirstActionButton.js
@@ -0,0 +1,111 @@
+import React from "react";
+import styled from "styled-components/macro";
+
+const ActionButton = styled.button`
+  position: relative;
+  width: 200px;
+  height: 200px;
+  border: none;
+  border-radius: 50%;
+  background-color: var(--decision-primary-color);
+  :hover {
+    -webkit-animation: heartbeat 2.5s ease-in-out infinite both;
+    animation: heartbeat 1.5s ease-in-out infinite both;
+
+    @-webkit-keyframes heartbeat {
+      from {
+        -webkit-transform: scale(1);
+        transform: scale(1);
+        -webkit-transform-origin: center center;
+        transform-origin: center center;
+        -webkit-animation-timing-function: ease-out;
+        animation-timing-function: ease-out;
+      }
+      10% {
+        -webkit-transform: scale(0.91);
+        transform: scale(0.91);
+        -webkit-animation-timing-function: ease-in;
+        animation-timing-function: ease-in;
+      }
+      17% {
+        -webkit-transform: scale(0.98);
+        transform: scale(0.98);
+        -webkit-animation-timing-function: ease-out;
+        animation-timing-function: ease-out;
+      }
+      33% {
+        -webkit-transform: scale(0.87);
+        transform: scale(0.87);
+        -webkit-animation-timing-function: ease-in;
+        animation-timing-function: ease-in;
+      }
+      45% {
+        -webkit-transform: scale(1);
+        transform: scale(1);
+        -webkit-animation-timing-function: ease-out;
+        animation-timing-function: ease-out;
+      }
+    }
+    @keyframes heartbeat {
+      from {
+        -webkit-transform: scale(1);
+        transform: scale(1);
+        -webkit-transform-origin: center center;
+        transform-origin: center center;
+        -webkit-animation-timing-function: ease-out;
+        animation-timing-function: ease-out;
+      }
+      10% {
+        -webkit-transform: scale(0.91);
+        transform: scale(0.91);
+        -webkit-animation-timing-function: ease-in;
+        animation-timing-function: ease-in;
+      }
+      17% {
+        -webkit-transform: scale(0.98);
+        transform: scale(0.98);
+        -webkit-animation-timing-function: ease-out;
+        animation-timing-function: ease-out;
+      }
+      33% {
+        -webkit-transform: scale(0.87);
+        transform: scale(0.87);
+        -webkit-animation-timing-function: ease-in;
+        animation-timing-function: ease-in;
+      }
+      45% {
+        -webkit-transform: scale(1);
+        transform: scale(1);
+        -webkit-animation-timing-function: ease-out;
+        animation-timing-function: ease-out;
+      }
+    }
+  }
+`;
+
+const Outline = styled.div`
+  position: relative;
+  width: 200px;
+  height: 200px;
+  border: 1px solid #607a94;
+  border-radius: 50%;
+  opacity: 3;
+  z-index: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  span {
+    color: #fbfbfb;
+  }
+`;
+
+export default function FirstActionButton() {
+  return (
+    <ActionButton>
+      <Outline>
+        <span>Was gibts heute zu essen?</span>
+      </Outline>
+    </ActionButton>
+  );
+}
---
+++ client/src/stories/FirstActionButton.stories.js
@@ -0,0 +1,10 @@
+import React from "react";
+import FirstActionButton from "../components/FirstActionButton";
+
+export default {
+  title: "Components/FirstActionButton",
+  parameters: { layout: "centered" },
+  component: FirstActionButton,
+};
+
+export const FirstAction = () => <FirstActionButton />;
