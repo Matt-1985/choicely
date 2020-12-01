c1bec3e0da8ceff6ff213a6d2ecd9455cbc27b5b
Matt-1985 <blatsche@web.de>
2020-12-01 16:30:20 +0100

    💄 ✨ Build startpage

---
+++ client/src/stories/StartPage.stories.js
@@ -0,0 +1,9 @@
+import React from "react";
+import StartPage from "../pages/StartPage";
+
+export default {
+  title: "Pages/StartPage",
+  component: StartPage,
+};
+
+export const Start = (args) => <StartPage {...args} />;
