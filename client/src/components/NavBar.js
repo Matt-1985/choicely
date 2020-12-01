1faac690837125287e2aa4cf4475aa362786028d
Matt-1985 <blatsche@web.de>
2020-12-01 14:34:14 +0100

    ✨ Add Bottom Navigation

---
+++ client/src/components/NavBar.js
@@ -0,0 +1,34 @@
+import React from "react";
+import styled from "styled-components/macro";
+import BackButton from "../stories/assets/back_btn.svg";
+import RefreshButton from "../stories/assets/refresh_btn.svg";
+
+const Navigation = styled.footer`
+  border: 1px solid grey;
+  height: 75px;
+  width: 20vw;
+  padding: 5px;
+  position: fixed;
+  bottom: 0;
+  display: flex;
+  justify-content: space-evenly;
+
+  img {
+    :active {
+      opacity: 0.6;
+    }
+  }
+`;
+
+export default function BottomNavigation() {
+  return (
+    <Navigation>
+      <a href="/">
+        <img src={BackButton} alt="back" />
+      </a>
+      <a href="/">
+        <img src={RefreshButton} alt="back" />
+      </a>
+    </Navigation>
+  );
+}
