089f8a58d0efab72f5357b9ad04cc0e23378f96e
Matt-1985 <blatsche@web.de>
2021-01-06 10:45:56 +0100

    🗑 Clean old storybook files

--- client/src/stories/DesicionPage.stories.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import DesicionPage from "../pages/DesicionPage";
-
-export default {
-  title: "Pages/DesicionPage",
-  component: DesicionPage,
-};
-
-export const SecondPage = (args) => <DesicionPage {...args} />;
